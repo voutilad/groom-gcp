daaa193ad69f3e929f6238b22191c89eee736f51
Dave Voutila <voutilad@gmail.com>
2020-06-09 17:38:57 -0400

    Add in Groom specific Cypher
    Also toggle the groom topic

--- udp/publisher.js
+++ udp/publisher.js
@@ -2,10 +2,37 @@ const dgram = require('dgram')
 const { PubSub } = require('@google-cloud/pubsub')
 
 // GCP specific vars
-const TOPIC = 'test-topic'
+const TOPIC = 'groom'
 const PROJECT = 'neo4j-se-team-201905'
 const KEY = '../secrets/neo4j-se-team-201905-208a5b2ddcc7.json'
-const CYPHER = `CREATE (d:Dave {isCool:true})`
+const CYPHER = `
+MERGE (frame:Frame {tic: event.frame.tic})
+    ON CREATE SET frame.millis = event.frame.millis
+CREATE (ev:Event {type: event.type, counter: event.counter})
+CREATE (ev)-[:OCCURRED_AT]->(frame)
+
+// Conditionally process Actor and Target
+FOREACH (thing IN [x IN [event.actor, event.target] WHERE x IS NOT NULL] |
+    MERGE (actor:Actor {id: thing.id}) ON CREATE SET actor.type = thing.type
+    MERGE (subsector:SubSector {id: thing.position.subsector})
+    CREATE (actorState:State)
+    SET actorState.position = point(thing.position),
+        actorState.angle = thing.position.angle,
+        actorState.health = thing.health,
+        actorState.armor = thing.armor,
+        actorState.actorId = thing.id
+    CREATE (actorState)-[:IN_SUBSECTOR]->(subsector)
+
+    // Hacky logic...hold your nose
+    FOREACH (_ IN CASE thing.id WHEN event.actor.id
+        THEN [1] ELSE [] END | CREATE (actorState)-[:ACTOR_IN]->(ev))
+    FOREACH (_ IN CASE thing.id WHEN event.target.id
+        THEN [1] ELSE [] END | CREATE (actorState)-[:TARGET_IN]->(ev))
+    FOREACH (_ IN CASE thing.type WHEN 'player'
+        THEN [1] ELSE [] END | SET actor:Player, actorState:PlayerState)
+    FOREACH (_ IN CASE thing.type WHEN 'player'
+        THEN [] ELSE [1] END | SET actor:Enemy, actorState:EnemyState)
+)`
 
 // Groom specific vars
 const WINDOW_SIZE = 33
