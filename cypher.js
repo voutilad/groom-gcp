65e70fabd5f5ef834d8f1cace827bfa18c3007b3
Dave Voutila <voutilad@gmail.com>
2020-10-07 15:59:27 -0400

    cypher is out of date, not using sessions properly
    Update with latest from other Groom project, setting the session from
    the event object on the Actor and SubSector.
    
    Fix session reference in the threading queries.

--- cypher.js
+++ cypher.js
@@ -14,28 +14,27 @@ MERGE (frame:Frame {tic: event.frame.tic, session: event.session})
     ON CREATE SET frame.millis = event.frame.millis
 CREATE (ev:Event {type: event.type, counter: event.counter})
 CREATE (ev)-[:OCCURRED_AT]->(frame)
-
 // Conditionally process Actor and Target
 FOREACH (thing IN [x IN [event.actor, event.target] WHERE x IS NOT NULL] |
-    MERGE (actor:Actor {id: thing.id}) ON CREATE SET actor.type = thing.type
-    MERGE (subsector:SubSector {id: thing.position.subsector})
+    MERGE (actor:Actor {id: thing.id, session: event.session})
+        ON CREATE SET actor.type = thing.type
+    MERGE (subsector:SubSector {id: thing.position.subsector, session: event.session})
     CREATE (actorState:State)
     SET actorState.position = point(thing.position),
         actorState.angle = thing.position.angle,
         actorState.health = thing.health,
         actorState.armor = thing.armor,
         actorState.actorId = thing.id,
-        actorState.actorSession = thing.session
+        actorState.actorSession = event.session
     CREATE (actorState)-[:IN_SUBSECTOR]->(subsector)
-
     // Hacky logic...hold your nose
     FOREACH (_ IN CASE thing.id WHEN event.actor.id
         THEN [1] ELSE [] END | CREATE (actorState)-[:ACTOR_IN]->(ev))
     FOREACH (_ IN CASE thing.id WHEN event.target.id
         THEN [1] ELSE [] END | CREATE (actorState)-[:TARGET_IN]->(ev))
-    FOREACH (_ IN CASE thing.type WHEN 'player'
+    FOREACH (_ IN CASE thing.type WHEN "player"
         THEN [1] ELSE [] END | SET actor:Player, actorState:PlayerState)
-    FOREACH (_ IN CASE thing.type WHEN 'player'
+    FOREACH (_ IN CASE thing.type WHEN "player"
         THEN [] ELSE [1] END | SET actor:Enemy, actorState:EnemyState)
 )`
 
@@ -74,7 +73,7 @@ MATCH (a:Actor)-[r:CURRENT_STATE]->(old:State) DELETE r
 
 const currentStateUpdate = `
 MATCH (s:State) WHERE NOT (s)<-[:PREV_STATE]-()
-MATCH (a:Actor {id:s.actorId, session:s.sessionId})
+MATCH (a:Actor {id:s.actorId, session:s.actorSession})
 MERGE (a)-[:CURRENT_STATE]->(s)
 `
 
